ec7e2919e1b1ac39a1b178a88c239a97894b9e9e
Davydov Sasha <davydoff1@gmail.com>
2018-02-28 00:33:55 +0200

    profile blade some change

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -5,49 +5,41 @@
 <div class="container">
     <div class="row">
         <div class="col-md-5  toppad  pull-right col-md-offset-3 ">
-                            <a href="{{ route('logout') }}"
-                   onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                    Logout
-                </a>
+            <a href="{{ route('logout') }}"
+               onclick="event.preventDefault();
+               document.getElementById('logout-form').submit();">Logout</a>
 
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    {{ csrf_field() }}
-                </form>
-
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                {{ csrf_field() }}
+            </form>
 
             <br>
-
-                <script type="text/javascript">
-                    function getDate()
-                    {
-                        var date = new Date();
-                        var year = date.getFullYear();
-                        var month = date.getMonth();
-                        var day = date.getDay();
-                        var hours = date.getHours();
-                        var minutes = date.getMinutes();
-                        var seconds = date.getSeconds();
-                        if (month < 10)
-                        {
-                            month = '0' + month;
-                        }
-                        if (day < 10)
-                        {
-                            day = '0' + day;
-                        }
-                        if(seconds < 10)
-                        {
-                            seconds = '0' + seconds;
-                        }
-                        document.getElementById('timedisplay').innerHTML = day + '/' + month + '/' + year + "<br>" + hours + ':' + minutes + ':' + seconds;
+            <script type="text/javascript">
+                function getDate() {
+                    var date = new Date();
+                    var year = date.getFullYear();
+                    var month = date.getMonth();
+                    var day = date.getDay();
+                    var hours = date.getHours();
+                    var minutes = date.getMinutes();
+                    var seconds = date.getSeconds();
+                    if (month < 10) {
+                        month = '0' + month;
                     }
+                    if (day < 10) {
+                        day = '0' + day;
+                    }
+                    if (seconds < 10) {
+                        seconds = '0' + seconds;
+                    }
+                    document.getElementById('timedisplay').innerHTML = day + '/' + month + '/' + year + "<br>" + hours + ':' + minutes + ':' + seconds;
+                }
                 setInterval(getDate, 0);
             </script>
 
             <div id="timedisplay"></div>
         </div>
-        <div class="col-xs-10 col-sm-10 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad" >
+        <div class="col-xs-10 col-sm-10 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad">
 
             <div class="panel panel-info">
                 <div class="panel-heading">
@@ -55,24 +47,22 @@
                 </div>
                 <div class="panel-body">
                     <div class="row">
-                       <div class=" col-md-10 col-lg-10 ">
+                        <div class=" col-md-10 col-lg-10 ">
                             <table class="table table-user-information">
                                 <tbody>
-                                <tr><td>Department:</td><td>Programming</td>
-                                </tr><tr><td>Hire date:</td><td>06/23/2013</td>
-                                </tr>
-
+                                <tr><td>Email:</td><td>{{Auth::user()-> email}}</td></tr>
+                                <tr><td>Registration date:</td><td>{{Auth::user()-> created_at}}</td></tr>
                                 </tbody>
                             </table>
-
-                       </div>
+                        </div>
                     </div>
                 </div>
                 <div class="panel-footer">
-                    <a data-original-title="Broadcast Message" data-toggle="tooltip" type="button" class="btn btn-sm btn-primary"><i class="glyphicon glyphicon-envelope"></i></a>
+                    <a data-original-title="Broadcast Message" data-toggle="tooltip" type="button"
+                       class="btn btn-sm btn-primary"><i class="glyphicon glyphicon-envelope"></i></a>
                     <span class="pull-right">
-                            <a href="edit.html" data-original-title="Edit this user" data-toggle="tooltip" type="button" class="btn btn-sm btn-warning"><i class="glyphicon glyphicon-edit"></i></a>
-
+                            <a href="edit.html" data-original-title="Edit this user" data-toggle="tooltip" type="button"
+                               class="btn btn-sm btn-warning"><i class="glyphicon glyphicon-edit"></i></a>
                         </span>
                 </div>
 
